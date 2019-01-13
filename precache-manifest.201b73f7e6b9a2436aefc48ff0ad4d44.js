1153454f981a820f5dc2a0bbbaf83589d0c9db52
Vladimir <genoconed@gmail.com>
2019-01-13 19:12:11 +0300

    Updates

---
+++ precache-manifest.201b73f7e6b9a2436aefc48ff0ad4d44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d651a23faa88b4c0917",
+    "url": "/admin-page-fund/static/js/runtime~main.0d651a23.js"
+  },
+  {
+    "revision": "7a1b936f049f7ecf410b",
+    "url": "/admin-page-fund/static/js/main.7a1b936f.chunk.js"
+  },
+  {
+    "revision": "841bc52ae3ed79d4dfbd",
+    "url": "/admin-page-fund/static/js/1.841bc52a.chunk.js"
+  },
+  {
+    "revision": "7a1b936f049f7ecf410b",
+    "url": "/admin-page-fund/static/css/main.31658a96.chunk.css"
+  },
+  {
+    "revision": "7fb14851b615d0028e7eb75f86d46cb1",
+    "url": "/admin-page-fund/index.html"
+  }
+];
